65824ab597dc7f35d4ac53899cb8780b37955cf1
navya053-byte <navyasri.are@slu.edu>
2025-04-23 03:46:44 +0000

    Lab-05

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-const express = require('express')
-const api = require('./api')
-const middleware = require('./middleware')
-const bodyParser = require('body-parser')
-
-
-// Set the port
-const port = process.env.PORT || 3000
-// Boot the app
-const app = express()
-// Register the public directory
-app.use(express.static(__dirname + '/public'));
-// register the routes
-app.use(bodyParser.json())
-app.use(middleware.cors)
-app.get('/', api.handleRoot)
-app.get('/products', api.listProducts)
-app.get('/products/:id', api.getProduct)
+@@ -19,6 +19,13 @@ app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+
+app.post('/orders', api.createOrder)
+app.get('/orders/:id', api.editOrder)
+app.get('/orders/:id', api.deleteOrder)
+app.post('/orders', api.listOrders)
+
+
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
-
